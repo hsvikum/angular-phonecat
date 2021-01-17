45be35b5c1a3053a4f910786300ad3bbb2bc81d9
Shalitha Vikum <hsvikum@gmail.com>
2021-01-17 23:58:40 +0530

    Added webpack and changed the ts config acording to https://github.com/jawache/angularjs-migration

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./app/main.ts",
+    output: {
+        filename: "../app/bundle.js"
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    mode: "development"
+};
