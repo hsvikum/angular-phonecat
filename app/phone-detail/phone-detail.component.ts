ea1d0dd492c98dbe60d72d508befd55fd5c5e617
Shalitha Vikum <hsvikum@gmail.com>
2021-01-17 20:00:14 +0530

    Made the phone details controller typescript and converted the contoller to a class

---
+++ app/phone-detail/phone-detail.component.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+// Register `phoneDetail` component, along with its associated controller and template
+class PhoneDetailController {
+  phone: any;
+  mainImageUrl: string | undefined;
+
+  static $inject = ['$routeParams', 'Phone'];
+  constructor($routeParams: angular.route.IRouteParamsService, Phone: any) {
+    const phoneId = $routeParams.phoneId;
+    this.phone = Phone.get({phoneId}, (phone: any) => {
+      this.setImage(phone.images[0]);
+    });
+  }
+
+  setImage(imageUrl: string) {
+    this.mainImageUrl = imageUrl;
+  }
+}
+
+angular.
+  module('phoneDetail').
+  component('phoneDetail', {
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: PhoneDetailController
+  });
