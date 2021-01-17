296eb1d47073a5f3dd1dc64cfb90c6ec7943bb1e
Shalitha Vikum <hsvikum@gmail.com>
2021-01-17 19:31:27 +0530

    Converted the phone list controller to a class

---
+++ app/phone-list/phone-list.component.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+class PhoneListController {
+  phones: any[];
+  orderProp: string;
+  // query: string;
+
+  static $inject = ['Phone'];
+  constructor(Phone: any) {
+    this.phones = Phone.query();
+    this.orderProp = 'age';
+  }
+}
+
+angular.
+  module('phoneList').
+  component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: PhoneListController
+  });
